f5bc1182105073ce682cc1d388468ecf7071e32d
reut apel <reutapel88@gmail.com>
2015-11-26 11:39:43 +0200

    hw1: upload

---
+++ hw1/check.py
@@ -0,0 +1,151 @@
+import ex1
+import search
+import time
+
+def timeout_exec(func, targs=(), kwargs={}, timeout_duration=10, default=None):
+    """This function will spawn a thread and run the given function
+    using the args, kwargs and return the given default value if the
+    timeout_duration is exceeded.
+    """ 
+    import threading, ctypes
+    class InterruptableThread(threading.Thread):
+        def __init__(self):
+            threading.Thread.__init__(self)
+            self.result = default
+        def run(self):
+            try:
+                self.result = func(*targs, **kwargs)
+            except Exception as e:    
+                self.result = [-3, -3, e]
+        def _async_raise(cls, tid, excobj):
+            res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(excobj))
+            if res == 0:
+                raise ValueError("nonexistent thread id")
+            elif res > 1:
+                ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, 0)
+                raise SystemError("PyThreadState_SetAsyncExc failed")
+
+        def raise_exc(self, excobj):
+            assert self.isAlive(), "thread must be started"
+            for tid, tobj in threading._active.items():
+                if tobj is self:
+                    self._async_raise(tid, excobj)
+                    return
+        def terminate(self):
+            self.raise_exc(SystemExit)  
+            
+    it = InterruptableThread()
+    t1 = time.time()
+    it.start()
+    it.join(timeout_duration)
+
+    print "Start Thread execution"
+    try:
+        while  time.time() - t1 <= timeout_duration:
+            if not it.isAlive():
+                return it.result
+        it.terminate()
+        print "Thread killed by timeout"
+        return default
+    except Exception as e:
+        print "Thread killed by exeption"
+        return it.result
+    print "End Thread execution"
+
+def check_problem(p, search_method, timeout):    
+    """ Constructs a problem using ex1.create_bomberman_problem, 
+    and solves it using the given search_method with the given timeout.
+    Returns a list of [solution length, solution time, solution]
+    (-2, -2, None) means there was a timeout
+    (-3, -3, ERR) means there was some error ERR during search"""        
+    
+    
+    t1 = time.time()
+    s = timeout_exec(search_method, targs=[p], timeout_duration=timeout)
+    t2 = time.time()
+    
+    if isinstance(s, search.Node):
+        solve = s
+        solution = map(lambda n: n.action, solve.path()[::-1])[1:]
+        return [len(solution), t2 - t1, solution]
+    elif s is None:
+        return [-2, -2, None]
+    else:        
+        return s
+    
+
+def solve_problem(problem):
+    solved = 0
+         
+    try:
+        t_delta = time.time()
+        p = ex1.create_bomberman_problem(problem)
+        t_delta = time.time() - t_delta
+        print "Initialization took %s seconds." %t_delta
+    except Exception as e:
+        print "Error creating problem: ", e
+        return None
+    timeout = 60 - t_delta
+    result = check_problem(p, (lambda p: search.best_first_graph_search(p, p.h)), timeout)
+    if type(result) == 'list' and result[1] > 0:
+        result[1] += t_delta
+    print "GBFS", result
+
+    if result[2] != None:
+        solved = solved + 1
+    #result = check_problem(p, search.astar_search, timeout)
+    #print "A*   ", result        
+    #result = check_problem(p, search.breadth_first_graph_search, timeout)
+    #print "BFSg ", result
+    #result = check_problem(p, search.breadth_first_tree_search, timeout)
+    #print "BFSt ", result
+    #result = check_problem(p, search.depth_first_graph_search, timeout)
+    #print "DFSg ", result
+    #result = check_problem(p, search.depth_first_tree_search, timeout)
+    #print "DFSt ", result
+    #result = check_problem(p, search.iterative_deepening_search, timeout)
+    
+    print "GBFS Solved ", solved
+
+    
+def main():
+    print ex1.ids
+    game0 = (
+             (12,99,18),
+             (10,99,10),
+             (10,90,10),
+            )
+    
+    game1 = (
+             (12,10,10,10,15),
+             (10,10,99,99,99),
+             (10,10,90,10,10),
+             (13,10,90,10,18)
+            )
+    
+    game2 = (
+             (18,10,90,90,14),
+             (10,99,90,99,90),
+             (90,90,90,90,90),
+             (90,99,90,99,90),
+             (90,90,90,90,13)
+            )
+    
+    game3 = (
+             (12,10,10,10,10,10,10,10,10,10,15),
+             (10,99,99,99,99,99,99,99,99,99,10),
+             (10,99,10,90,10,10,10,10,90,90,10),
+             (10,99,90,10,90,10,10,10,10,99,10),
+             (10,99,10,90,10,90,10,10,10,99,10),
+             (10,99,10,10,90,10,90,10,10,99,10),
+             (10,99,10,10,10,90,10,90,10,99,10),
+             (10,99,10,10,10,10,90,10,90,99,10),
+             (10,99,10,10,10,10,10,90,10,99,10),
+             (10,99,18,10,10,10,10,10,90,99,10),
+             (10,99,99,99,99,99,99,99,99,99,10),
+             (13,10,10,10,10,10,10,10,10,10,14),
+            )
+    solve_problem(game0)
+    
+if __name__ == '__main__':
+    main()
---
+++ hw1/ex1.py
@@ -0,0 +1,191 @@
+import search
+import math
+
+ids = ["201316346", "201110376"]  # Feel your IDs
+
+
+class BombermanProblem(search.Problem):
+    """This class implements a Bomberman problem"""
+
+
+def __init__(self, initial):
+    """ Constructor only needs the initial state.
+    Don't forget to set the goal or implement the goal test"""
+    search.Problem.__init__(self, initial)
+    self.N = len(initial)
+    self.M = len(initial[1])
+    self.Monsters = {}
+    self.Walls = set()
+    self.Bomb = [False, [None, None] ]
+    for row in range (0,self.N):
+        for col in range (0,self.M):
+            cell= initial[row][col]
+            if cell == 90:
+                self.Walls.add((row, col))
+            elif cell == 18:
+                self.BMx = row
+                self.BMy = col
+            elif cell in range (12,16):
+                self.Monsters.update({cell:[row, col]})
+
+    def successor(self, state):
+        """Given a state, return a sequence of (action, state) pairs reachable
+        from this state. If there are many successors, consider an iterator
+        that yields the successors one at a time, rather than building them
+        all at once. Iterators will work fine within the framework."""
+        suc_list = []
+        for act in ['U', 'D', 'R', 'L', 'W', 'B', 'S']: #for each action, check what will be the state after we bomberman will do it/
+            suc_state = suc_successor(act, state)
+            if suc_state is not None: #if the action is legal --> insert the action+ new successor state to the successors list
+                suc_list.append((act, suc_state))
+        return suc_list
+
+
+    def suc_successor(self, act, state):
+      mut_state = [list(a) for a in state] #change the given state to mutable state
+
+        if act == 'U':
+            x = self.BMx
+            y = self.BMy
+            nx = (x-1)
+            ny = y
+            checked_state = do_checks(self,mut_state, x, y, nx, ny)
+            if not checked_state:
+                return None
+            return checked_state
+
+
+        elif act == 'D':
+            x = self.BMx
+            y = self.BMy
+            nx = (x+1)
+            ny = y
+            checked_state = do_checks(self,mut_state, x, y, nx, ny)
+            if not checked_state:
+                return None
+            return checked_state
+
+        elif act == 'R':
+            x = self.BMx
+            y = self.BMy
+            nx = x
+            ny = (y+1)
+            checked_state = do_checks(self,mut_state, x, y, nx, ny)
+            if not checked_state:
+                return None
+            return checked_state
+
+
+        elif act == 'L':
+            x = self.BMx
+            y = self.BMy
+            nx = x
+            ny = (y-1)
+            checked_state = do_checks(self,mut_state, x, y, nx, ny)
+            if not checked_state:
+                return None
+            return checked_state
+
+        elif act == 'W':
+
+
+        elif act == 'S':
+
+
+        elif act == 'B':
+
+
+
+        imut_state = (tuple(b) for b in mut_state ) #return the changed state into hashable mode
+        return imut_state
+
+
+    def in_bound(self, x, y): # checks if the wanted direction will end in boundaries
+        if 0 <= x < self.N and 0 <= y < self.M:
+            return True
+        return False
+
+    def is_wall(self, x, y):
+        if (x,y) in self.Walls:
+            return True
+        return False
+
+    def is_mons(self, x, y):
+        if [x, y] in self.Monsters.values():
+            return True
+        return False
+
+    def is_hwall(mut_state, x, y):
+        if mut_state[x][y] == 99:
+            return True
+        return False
+
+    def is_bomb(self, x, y):
+        if self.Bomb[0] == True:
+            if self.Bomb[1] == [x, y]:
+                return True
+        return False
+
+    def is_empty(mut_state, x, y):
+        if mut_state[x][y] == 10:
+            return True
+        return False
+
+    def move_mons():
+        return
+
+    def do_checks(self, mut_state, x, y, nx, ny): ## Q: can I save mut_state and just use self.
+        if not in_bound(self, nx, ny):
+            return False
+        if is_empty(mut_state, nx, ny):
+            mut_state[nx][ny] = 18
+            self.BMx = nx
+            self.BMy = ny
+            if mut_state[x][y] == 18:
+                mut_state[x][y] = 10
+            else:
+                mut_state[x][y] = 80
+                move_mons()
+                return mut_state
+        if is_mons(self, nx,ny):
+            return False
+##        if is_wall(self, nx,ny):
+##            return False
+##        if is_hwall(mut_state,nx, ny):
+##            return False
+##        if is_bomb(self, nx, ny):
+##            return False
+        else:
+            move_mons()
+            return mut_state
+
+
+
+
+
+
+    def h(self, node):
+        """ This is the heuristic. It get a node (not a state)
+        and returns a goal distance estimate"""
+        state = node.state
+        return None
+
+    def goal_test(self, state):
+        """Return True if the state is a goal.
+        State will be a goal only if Bomberman lives and all the monsters die"""
+        if self.BMx is not None and not bool(self.Monsters):
+            return True
+
+
+def create_bomberman_problem(game):
+    print "<<create_bomberman_problem"
+    """ Create a bomberman problem, based on the description.
+    game - matrix as it was described in the pdf file"""
+    print "PROBLEM: "
+    for row in game:
+        for c in row:
+            print c,
+        print
+    return BombermanProblem(game)
+
+
