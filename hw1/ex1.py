a04e9eab996f45da728b61a2ffb14ef6d3e373ca
reut apel <reutapel88@gmail.com>
2015-11-26 22:34:07 +0200

    1. add self. when calling to the class methods 2. add a different option to blow_moster - see in comments

--- hw1/ex1.py
+++ hw1/ex1.py
@@ -8,25 +8,25 @@ class BombermanProblem(search.Problem):
     """This class implements a Bomberman problem"""
 
 
-def __init__(self, initial):
-    """ Constructor only needs the initial state.
-    Don't forget to set the goal or implement the goal test"""
-    search.Problem.__init__(self, initial)
-    self.N = len(initial)
-    self.M = len(initial[1])
-    self.Monsters = {}
-    self.Walls = set()
-    self.Bomb = [False, [None, None]]
-    for row in range(0,self.N):
-        for col in range(0,self.M):
-            cell = initial[row][col]
-            if cell == 90:
-                self.Walls.add((row, col))
-            elif cell == 18:
-                self.BMx = row
-                self.BMy = col
-            elif cell in range(12,16):
-                self.Monsters.update({cell: [row, col]})
+    def __init__(self, initial):
+        """ Constructor only needs the initial state.
+        Don't forget to set the goal or implement the goal test"""
+        search.Problem.__init__(self, initial)
+        self.N = len(initial)
+        self.M = len(initial[1])
+        self.Monsters = {}
+        self.Walls = set()
+        self.Bomb = [False, [None, None]]
+        for row in range(0,self.N):
+            for col in range(0,self.M):
+                cell = initial[row][col]
+                if cell == 90:
+                    self.Walls.add((row, col))
+                elif cell == 18:
+                    self.BMx = row
+                    self.BMy = col
+                elif cell in range(12,16):
+                    self.Monsters.update({cell: [row, col]})
 
     def successor(self, state):
         """Given a state, return a sequence of (action, state) pairs reachable
@@ -48,7 +48,7 @@ def __init__(self, initial):
             y = self.BMy
             nx = (x-1)
             ny = y
-            checked_state = do_checks(self,mut_state, x, y, nx, ny)
+            checked_state = self.do_checks(self,mut_state, x, y, nx, ny)
             if not checked_state:
                 return None
             return checked_state
@@ -58,7 +58,7 @@ def __init__(self, initial):
             y = self.BMy
             nx = (x+1)
             ny = y
-            checked_state = do_checks(self,mut_state, x, y, nx, ny)
+            checked_state = self.do_checks(self,mut_state, x, y, nx, ny)
             if not checked_state:
                 return None
             return checked_state
@@ -68,7 +68,7 @@ def __init__(self, initial):
             y = self.BMy
             nx = x
             ny = (y+1)
-            checked_state = do_checks(self,mut_state, x, y, nx, ny)
+            checked_state = self.do_checks(self,mut_state, x, y, nx, ny)
             if not checked_state:
                 return None
             return checked_state
@@ -78,36 +78,36 @@ def __init__(self, initial):
             y = self.BMy
             nx = x
             ny = (y-1)
-            checked_state = do_checks(self,mut_state, x, y, nx, ny)
+            checked_state = self.do_checks(self,mut_state, x, y, nx, ny)
             if not checked_state:
                 return None
             return checked_state
 
         elif act == 'W':
-            move_mons()
-            imut_state = (tuple(b) for b in mut_state )
+            self.move_mons()
+            imut_state = (tuple(b) for b in mut_state)
             return imut_state
 
         elif act == 'S':
-            if self.Bomb[0] == True:
+            if self.Bomb[0]:
                 return None
             else:
                 self.Bomb[1] = [self.BMx, self. BMy]
                 mut_state[self.BMx][self.BMy] = 88
-                move_mons()
+                self.move_mons()
                 imut_state = (tuple(b) for b in mut_state)
                 return imut_state
 
         elif act == 'B':
-            if self.Bomb[0] == False:
+            if not self.Bomb[0]:
                 return None
-            if blow_bomber():
+            if self.blow_bomber():
                 return None
-            blow_walls(self, mut_state)
-            blow_mons(self, mut_state)
-            move_mons()
-                imut_state = (tuple(b) for b in mut_state)
-                return imut_state
+            self.blow_walls(self, mut_state)
+            self.blow_mons(self, mut_state)
+            self.move_mons()
+            imut_state = (tuple(b) for b in mut_state)
+            return imut_state
 
     def in_bound(self, x, y):
         if 0 <= x < self.N and 0 <= y < self.M:
@@ -124,29 +124,29 @@ def __init__(self, initial):
             return True
         return False
 
-    def is_hwall(mut_state, x, y):
+    def is_hwall(self, mut_state, x, y):
         if mut_state[x][y] == 99:
             return True
         return False
 
     def is_bomb(self, x, y):
-        if self.Bomb[0] == True:
+        if self.Bomb[0]:
             if self.Bomb[1] == [x, y]:
                 return True
         return False
 
-    def is_empty(mut_state, x, y):
+    def is_empty(self, mut_state, x, y):
         if mut_state[x][y] == 10:
             return True
         return False
 
-    def move_mons():
+    def move_mons(self):
         return
 
     def do_checks(self, mut_state, x, y, nx, ny): ## Q: can I save mut_state and just use self.
-        if not in_bound(self, nx, ny):
+        if not self.in_bound(self, nx, ny):
             return False
-        if is_empty(mut_state, nx, ny):
+        if self.is_empty(mut_state, nx, ny):
             mut_state[nx][ny] = 18
             self.BMx = nx
             self.BMy = ny
@@ -154,11 +154,11 @@ def __init__(self, initial):
                 mut_state[x][y] = 10
             else:
                 mut_state[x][y] = 80
-            move_mons()
+            self.move_mons()
             imut_state = (tuple(b) for b in mut_state )
             return imut_state
         else:
-            move_mons()
+            self.move_mons()
             imut_state = (tuple(b) for b in mut_state )
             return imut_state
 
@@ -197,10 +197,13 @@ def __init__(self, initial):
         x = self.Bomb[1][0]
         y = self.Bomb[1][1]
         for key, item in self.Monsters.items():
+            # if item in ([x+1, y],[x+1, y], [x, y+1],[x, y-1]):
+            #     mut_state[item] = 10
+            #     del self.Monsters[key]
             if item == [x+1, y]:
                 mut_state[x+1][y] = 10
                 del self.Monsters[key]
-            elif item == [x-1, y]:
+            elif item == [x+1, y]:
                 mut_state[x-1][y] = 10
                 del self.Monsters[key]
             elif item == [x, y+1]:
@@ -211,13 +214,11 @@ def __init__(self, initial):
                 del self.Monsters[key]
         return
 
-
-
-
     def h(self, node):
         """ This is the heuristic. It get a node (not a state)
         and returns a goal distance estimate"""
         state = node.state
+
         return None
 
     def goal_test(self, state):
