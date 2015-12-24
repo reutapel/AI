49db16f37155258c503a38784c11da4b2f467471
reut apel <reutapel88@gmail.com>
2015-12-24 21:54:43 +0200

    hw2: first update

--- hw2/ex2.py
+++ hw2/ex2.py
@@ -9,7 +9,12 @@ class Controller:
     def __init__(self, board, steps):
         """Initialize controller for given game board and number of steps.
         This method MUST terminate within the specified timeout."""
-        
+        self.BMx = None
+        self.BMy = None
+        self.N = len(board)
+        self.M = len(board[1])
+        self.steps = steps
+
     
     def choose_next_move(self, board, n, reward):
         "Choose next action for Bomberman given the current state of the board."
